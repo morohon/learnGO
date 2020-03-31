8322a8199a5932a2c4b10c155bb6d042f3de1956
Kirill Verevkin <kira795@yandex.ru>
2020-03-31 12:44:52 +0300

    loops

---
+++ lesson4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Строка №", i)
+	}
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Println(i, j)
+		}
+	}
+}
